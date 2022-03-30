d469a81759263c767dc14f2257e0f8f6adb06de0
Jonay Méndez Márquez <alu0101347141@ull.edu.es>
2022-03-30 09:56:51 +0000

    Terminado ejercicio

--- src/ejercicio.ts
+++ src/ejercicio.ts
@@ -2,16 +2,14 @@
  * Clase RandomNumber
  */
 export class RandomNumber {
-  private numero: number;
   private static randomNumberInstance: RandomNumber;
   /**
    * Constructor clase RandomNumber
    */
   private constructor() {
-    this.numero = Math.random();
   }
   /**
-   *
+   * "Getter" de la única instancia de la clase singleton
    * @return {RandomNumber}
    */
   public static getRandomNumberInstance(): RandomNumber {
@@ -25,29 +23,63 @@ export class RandomNumber {
    * @return {number}
    */
   zeroToOne(): number {
-    this.numero = Math.random();
-    return this.numero;
+    return Math.random();
   }
   /**
-   *
+   * Número flotante aleatorio entre un número min y otro max
    * @param {number} min
    * @param {number} max
    * @return {number}
    */
   minToMaxFloat(min: number, max: number): number {
-    this.numero = Math.random() * (max - min) + min;
-    return this.numero;
+    return Math.random() * (max - min) + min;
   }
   /**
-   *
+   * Número entero aleatorio entre un número min y otro max
    * @param {number} min
    * @param {number} max
    * @return {number}
    */
   minToMaxInt(min: number, max: number): number {
-    this.numero = Math.floor(Math.random() * (max - min)) + min;
-    return this.numero;
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 }
 
+/**
+ * Clase NumberCollection
+ */
+export class NumberCollection implements Iterable<number> {
+  private numeros: Set<number>;
 
+  /**
+   * Constructor de la clase NumberCollection
+   * @param {number[]} numeros
+   */
+  constructor(numeros: number[]) {
+    this.numeros = new Set(numeros);
+  }
+
+  /**
+   * Función para añadir un número a la colección
+   * @param {number} newNumber
+   */
+  addNumber(newNumber: number) {
+    this.numeros.add(newNumber);
+  }
+
+  /**
+   * Getter de la cantidad de números de la colección
+   * @return {number}
+   */
+  getNumberOfNumbers(): number {
+    return this.numeros.size;
+  }
+
+  /**
+   * Propiedad symbol.iterator
+   * @return {Iterator<number>}
+   */
+  [Symbol.iterator](): Iterator<number> {
+    return this.numeros.values();
+  }
+}
